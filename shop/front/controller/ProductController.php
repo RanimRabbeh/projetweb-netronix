19220d581b7edd206192b7ec971a5977c2cf374a
aminebentrad <131662995+aminebentrad@users.noreply.github.com>
2024-12-10 01:38:51 +0100

    Update ProductController.php

--- shop/front/controller/ProductController.php
+++ shop/front/controller/ProductController.php
@@ -21,15 +21,40 @@ class ProductController {
     }
 
     public function getAllProducts() {
-        $sql = "SELECT Nom, Prix, Quantite, Photo FROM produits";
+        $sql = "SELECT IdProduit, Nom, Prix, Quantite, Photo FROM produits";
         $stmt = $this->db->query($sql);
         $products = [];
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             $photoData = base64_encode($row['Photo']);
-            $products[] = new Product($row['Nom'], $row['Prix'], $row['Quantite'], $photoData);
+            $products[] = new Product([
+                'id' => $row['IdProduit'],
+                'nom' => $row['Nom'],
+                'prix' => $row['Prix'],
+                'quantite' => $row['Quantite'],
+                'photo' => $photoData
+            ]);
         }
         return $products;
     }
-    
+
+    public function getProductById($id) {
+        $sql = "SELECT IdProduit, Nom, Prix, Quantite, Photo FROM produits WHERE IdProduit = :id";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute(['id' => $id]);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($row) {
+            $photoData = base64_encode($row['Photo']);
+            return new Product([
+                'id' => $row['IdProduit'],
+                'nom' => $row['Nom'],
+                'prix' => $row['Prix'],
+                'quantite' => $row['Quantite'],
+                'photo' => $photoData
+            ]);
+        } else {
+            return null;
+        }
+    }
 }
 ?>
